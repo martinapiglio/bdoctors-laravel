1d7540f376130327f58c2cee7f912e46dedaa67c
manuelnones <manuelnones99@yahoo.com>
2023-06-27 15:52:36 +0200

    feat: add title in dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -11,22 +11,16 @@
         </h1>
     </div>
     @else
-    <div>
-        <a href="{{route('admin.details.show', $detail->slug)}}">mostra i dati del tuo profilo</a><br>
-        <a href="{{route('admin.details.edit', $detail->slug)}}">modifica i dati del tuo profilo</a><br>
-        <a href="{{route('admin.messages.index', $detail->slug)}}">Vedi i tuoi messaggi</a><br>
-        <a href="{{route('admin.reviews.index', $detail->slug)}}">vedi le tue recensioni</a><br>
-        <a href="{{route('admin.votes.index', $detail->slug)}}">vedi i tuoi voti</a><br>
-    </div>
-    
-    <div>
-        <a href="{{route('admin.sponsorships.index')}}">Vai alle sponsorizzazioni</a>
-    </div>
         <div class="sub-nav d-flex justify-content-evenly align-items-center">
-            <a href="{{route('admin.details.show', $detail->slug)}}">mostra i dati del tuo profilo</a><br>
-            <a href="{{route('admin.details.edit', $detail->slug)}}">modifica i dati del tuo profilo</a><br>
+            <a href="{{route('admin.details.show', $detail->slug)}}">Mostra i dati del tuo profilo</a><br>
+            <a href="{{route('admin.details.edit', $detail->slug)}}">Modifica i dati del tuo profilo</a><br>
+            <a href="{{route('admin.messages.index', $detail->slug)}}">Vedi i tuoi messaggi</a><br>
+            <a href="{{route('admin.reviews.index', $detail->slug)}}">Vedi le tue recensioni</a><br>
+            <a href="{{route('admin.votes.index', $detail->slug)}}">Vedi i tuoi voti</a><br>
             <a href="{{route('admin.sponsorships.index')}}">Vai alle sponsorizzazioni</a>
         </div>
+        <h1 class="text-hello text-center pt-5">Ciao {{$user->name}}</h1>
+        <h4 class="text-hello text-center">questa è la tua area personale, qui puoi gestire il tuo profilo.</h4>
         <div class="main-sec container py-5">
             <div class="jumbo-cont w-100 d-flex justify-content-center">
                 <img src="{{ asset("images/dottorireal.jpg") }}" alt="dottorini">
