f5bf9b9f00d8944cbdce2a437a46dab39bb093bc
soniagaber <soniagaber90@gmail.com>
2023-06-27 18:17:33 +0200

    show messaggi e show recensioni

--- resources/views/admin/messages/show.blade.php
+++ resources/views/admin/messages/show.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container py-5">
+<div id="show-mess" class="container py-5">
     
     <h1>Il tuo messaggio</h1>
     <h4> Oggetto: {{ $message->subject }} </h4>
-    <h6> Nome: {{ $message->name }}</h6>
-    <h6> Email: {{ $message->email }}</h6>
+    <h6> Nome: <i>{{ $message->name }}</i></h6>
+    <h6> Email: <i>{{ $message->email }}</i></h6>
     <p>{{ $message->message }}</p>
 
 
-    <a href=" {{ route('admin.messages.index')}} ">Torna a tutti i messaggi</a>
+    <button class="btn"><a href=" {{ route('admin.messages.index')}} ">Torna a tutti i messaggi</a></button> 
 
 </div>
 
--- resources/views/admin/reviews/show.blade.php
+++ resources/views/admin/reviews/show.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container py-5">
+<div id="show-recensioni" class="container py-5 ">
 
     @if($review != null)
     <h1>La tua recensione</h1>
-    <h6> Nome: {{ $review->name }}</h6>
+    <h6> Nome: <i>{{ $review->name }}</i></h6>
     <p>{{ $review->description }}</p>
 
     @else
     <div>non hai recensioni</div>
     @endif
 
-    <a href=" {{ route('admin.reviews.index')}} ">Torna a tutte le recensioni</a>
+    <button class="btn"><a href=" {{ route('admin.reviews.index')}} ">Torna a tutte le recensioni</a></button> 
 
 </div>
 
