b5b4b9ef5f8f5b0f5321d1b62340b9a5fd2f2bcb
martinapiglio <martina.piglio@gmail.com>
2023-06-29 09:03:48 +0200

    feat: title style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,16 +2,16 @@
 @section('content')
 
 <div class="benvenuto d-flex flex-column justify-content-center gap-4">
-    <div class="bd-cont d-none d-md-block ms-5">
+    <div class="bd-cont d-none d-md-block ms-5 title-big">
         BDoctors
     </div>
-    <div class="bd-cont-sm d-block d-md-none ms-5">
+    <div class="bd-cont-sm d-block d-md-none ms-5 title-big">
         BDoctors
     </div>
-    <div class="altro-cnt d-none d-md-block text-center">
+    <div class="altro-cnt d-none d-md-block text-center subtitle">
         La Piattaforma per professionisti come te
     </div>
-    <div class="altro-cnt-sm d-md-none text-center">
+    <div class="altro-cnt-sm d-md-none text-center subtitle">
         La Piattaforma per professionisti come te
     </div>
 </div>
