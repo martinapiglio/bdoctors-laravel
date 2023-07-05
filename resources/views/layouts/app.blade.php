7b21a647a6acb85d676fc2a7873c91e924bd7e45
martinapiglio <martina.piglio@gmail.com>
2023-07-05 11:14:10 +0200

    fix: doctor profile desc size + responsive

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Bdoctors') }}</title>
     {{-- ChartJS --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js" charset="utf-8"></script>
 
